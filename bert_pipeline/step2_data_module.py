1c7a666b8d469f31955bfb17b94c8eedd1b4fe69
martin-batista <mbatista.dev@gmail.com>
2022-07-17 19:33:07 -0300

    Added seed_everything to validation split.

---
+++ bert_pipeline/step2_data_module.py
@@ -0,0 +1,7 @@
+from clearml import Task
+import pandas as pd
+import numpy as np
+
+PROJECT_NAME = 'sentiment-classification-rtmovies5c'
+
+# task = Task.init(project_name=PROJECT_NAME, task_name='BERT_pipeline_2 data_module')
