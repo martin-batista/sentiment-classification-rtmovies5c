39bc891d1e295628546e5821ee35cf050c38c676
Martin Batista <mbatista.dev@gmail.com>
2022-07-18 01:42:33 -0300

    Fixed pipeline set to dict for parameters.

--- pl_lightning_pipe/pipeline_controller.py
+++ pl_lightning_pipe/pipeline_controller.py
@@ -36,8 +36,8 @@ pipe.add_step(
     name = 'data_split',
     base_task_name='LitTransformers_pipe_1 - train/val split',
     base_task_project=PROJECT_NAME,
-    parameter_override={'General/seed', '${pipeline.seed}',
-                        'General/validation_split', '${pipeline.validation_split}'} # type: ignore
+    parameter_override={'General/seed': '${pipeline.seed}',
+                        'General/validation_split': '${pipeline.validation_split}'} # type: ignore
 )
 
 pipe.add_step(
@@ -45,15 +45,15 @@ pipe.add_step(
     base_task_name='LitTransformers_pipe_2 - train_model',
     base_task_project=PROJECT_NAME,
     parents=['data_split'],
-    parameter_override={'General/seed', '${pipeline.seed}',
-                        'General/pre_trained_model', '${pipeline.pre_trained_model}',
-                        'General/batch_size', '${pipeline.batch_size}',
-                        'General/max_length', '${pipeline.max_length}',
-                        'General/lr', '${pipeline.lr}',
-                        'General/freeze_backbone', '${pipeline.freeze_backbone}',
-                        'General/num_epochs', '${pipeline.num_epochs}',
-                        'General/accelerator', '${pipeline.accelerator}',
-                        'General/devices', '${pipeline.devices}'} # type: ignore
+    parameter_override={'General/seed': '${pipeline.seed}',
+                        'General/pre_trained_model': '${pipeline.pre_trained_model}',
+                        'General/batch_size': '${pipeline.batch_size}',
+                        'General/max_length': '${pipeline.max_length}',
+                        'General/lr': '${pipeline.lr}',
+                        'General/freeze_backbone': '${pipeline.freeze_backbone}',
+                        'General/num_epochs': '${pipeline.num_epochs}',
+                        'General/accelerator': '${pipeline.accelerator}',
+                        'General/devices': '${pipeline.devices}'} # type: ignore
 )
 
 if __name__ == '__main__':
