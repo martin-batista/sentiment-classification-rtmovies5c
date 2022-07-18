c9f6f891024c55df5c520a3675abf85fc7dacace
Martin Batista <mbatista.dev@gmail.com>
2022-07-18 04:08:52 -0300

    Added PyYAML requirement.

--- pl_lightning_pipe/train_val_split.py
+++ pl_lightning_pipe/train_val_split.py
@@ -7,7 +7,11 @@ from scipy.stats import wasserstein_distance
 
 PROJECT_NAME = 'sentiment-classification-rtmovies5c'
 
-task = Task.init(project_name=PROJECT_NAME, task_name='LitTransformers train_val_split')
+task = Task.init(project_name=PROJECT_NAME, 
+                 task_name='train_val_split',
+                 task_type='data_processing', #type: ignore 
+                 )
+task.add_requirements("PyYAML")
 
 parameters = {
     'dataset_id': '8fe0f01e7c9540ac8b94ddbc84ac7ecb',
@@ -69,7 +73,7 @@ def log_histogram(task, df_1, df_2, df_3, title, name_1, name_2, name_3):
     )
 
 
-def main(task=task, parameters=parameters):
+def main(parameters=parameters):
     pl.seed_everything(parameters['seed'])
     train, test_data = get_train_test_data(parameters['dataset_id'])
     train_data, validation_data = train_validation_split(train, parameters['validation_split'], task)
@@ -91,18 +95,18 @@ def main(task=task, parameters=parameters):
     task.get_logger().report_table(title='Validation examples',series='pandas DataFrame',iteration=0,table_plot=validation_data)
     task.get_logger().report_table(title='Test examples',series='pandas DataFrame',iteration=0,table_plot=test_data)
 
-    task.get_logger().report_single_value('Train size', len(train_data))  # type: ignore
-    task.get_logger().report_single_value('Validation size', len(validation_data))  # type: ignore
-    task.get_logger().report_single_value('Test size', len(test_data))  # type: ignore
+    # task.get_logger().report_single_value('Train size', len(train_data)) 
+    # task.get_logger().report_single_value('Validation size', len(validation_data)) 
+    # task.get_logger().report_single_value('Test size', len(test_data))
 
-    task.get_logger().report_single_value('Train sentences', train_data['SentenceId'].nunique())  # type: ignore
-    task.get_logger().report_single_value('Validation sentences', validation_data['SentenceId'].nunique())  # type: ignore
-    task.get_logger().report_single_value('Test sentences', test_data['SentenceId'].nunique())  # type: ignore
+    # task.get_logger().report_single_value('Train sentences', train_data['SentenceId'].nunique())
+    # task.get_logger().report_single_value('Validation sentences', validation_data['SentenceId'].nunique())  
+    # task.get_logger().report_single_value('Test sentences', test_data['SentenceId'].nunique()) 
 
-    task.get_logger().report_single_value('Wasserstein train/valid', round(w_distance_train_valid,5))  # type: ignore
-    task.get_logger().report_single_value('Wasserstein train/test', round(w_distance_train_test,5))  # type: ignore
-    task.get_logger().report_single_value('Wasserstein test/valid', round(w_distance_test_valid,5))  # type: ignore
-    task.get_logger().report_single_value('Mean Wasserstein distance', round(mean_w_distance,5))  # type: ignore
+    # task.get_logger().report_single_value('Wasserstein train/valid', round(w_distance_train_valid,5))  
+    # task.get_logger().report_single_value('Wasserstein train/test', round(w_distance_train_test,5))  
+    # task.get_logger().report_single_value('Wasserstein test/valid', round(w_distance_test_valid,5)) 
+    # task.get_logger().report_single_value('Mean Wasserstein distance', round(mean_w_distance,5))  
 
 
     log_histogram(task, train_data, validation_data, test_data, 
