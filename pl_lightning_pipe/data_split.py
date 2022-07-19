9415806cb9ad8090544d2c379fbd3437b249d4d6
martin-batista <mbatista.dev@gmail.com>
2022-07-19 18:21:19 -0300

    Added task to gpu queue.

--- pl_lightning_pipe/data_split.py
+++ pl_lightning_pipe/data_split.py
@@ -82,7 +82,7 @@ def main():
     }
 
     task.connect(parameters)
-    task.execute_remotely('default')
+    task.execute_remotely('GPU')
 
     pl.seed_everything(parameters['seed'])
     train, test_data = get_train_test_data(parameters['dataset_id'])
