2cb157506b0b7e1a9e2f055b51c5727800c85c8d
martin-batista <mbatista.dev@gmail.com>
2022-07-20 22:38:22 -0300

    Stratified data loader.

--- pl_lightning_pipe/train_model.py
+++ pl_lightning_pipe/train_model.py
@@ -1,36 +1,42 @@
 #https://www.kaggle.com/code/kickitlikeshika/bert-for-sentiment-analysis-5th-place-solution
 
+# %%
+
 from clearml import Task
 import os
 from pathlib import Path
-import torch
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import StratifiedShuffleSplit
+
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
-from torch import Tensor
 from transformers import AutoTokenizer # type: ignore
 from lightning_transformers.task.nlp.text_classification import (
     TextClassificationDataModule,
 )
 from pipe_conf import PROJECT_NAME
 from pytorch_lightning.loggers import TensorBoardLogger
-from torch import nn
-from torch.utils.data import DataLoader
-from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+
+import torch
+from torch import nn, Tensor
+from torch.utils.data import DataLoader, Dataset, TensorDataset, DataLoader, Sampler
 from torchmetrics import Accuracy, ConfusionMatrix # type: ignore
+
 import pytorch_lightning as pl
 from transformers import AutoModel, AutoConfig, AutoTokenizer # type: ignore
+
 
 # from torch.utils.tensorboard import SummaryWriter
 
 # Task.add_requirements('requirements.txt')
-Task.add_requirements('requirements.txt')
-task = Task.init(project_name=PROJECT_NAME, 
-                task_name='train_model',
-                task_type='training', #type: ignore 
-                )
+# Task.add_requirements('requirements.txt')
+# task = Task.init(project_name=PROJECT_NAME, 
+#                 task_name='train_model',
+#                 task_type='training', #type: ignore 
+#                 )
 
-task.execute_remotely('GPU')
+# task.execute_remotely('GPU')
 parameters = {
         'validation_split': 0.1,
         'seed': 42,
@@ -43,10 +49,56 @@ parameters = {
         'devices': 'auto',
     }
 
-task.connect(parameters)
+# task.connect(parameters)
 
+class TokenizeDataset(Dataset):
+    def __init__(self, df, max_len, model_str, eval=False):
+        self.max_len = max_len
+        self.eval = eval
+        self.text = df['text'].tolist()
+        if not self.eval:
+            self.labels = df['label'].values
+            
+        tokenizer = AutoTokenizer.from_pretrained(model_str, trust_remote_code=True)
+        self.encode = tokenizer.batch_encode_plus(
+            self.text,
+            padding='max_length',
+            max_length=self.max_len,
+            truncation=True,
+            return_attention_mask=True
+        )
+        
+    def __getitem__(self, i):
+        input_ids = torch.tensor(self.encode['input_ids'][i]) # type: ignore
+        attention_mask = torch.tensor(self.encode['attention_mask'][i]) # type: ignore
+        
+        if self.eval:
+            return (input_ids, attention_mask)
 
-class BertBase(pl.LightningModule):
+        sentiments = self.labels[i]
+        return (input_ids, attention_mask, sentiments)
+    
+    def __len__(self):
+        return len(self.text)
+
+class StratifiedSampler(Sampler):
+    def __init__(self, y, batch_size):
+      self.y = y
+      self.batch_size = batch_size
+      self.n_splits = int(np.ceil(len(y) / batch_size))
+      self.n_classes = len(np.unique(y))
+      self.n_samples_per_split_per_class = int(np.ceil(len(y) / self.n_splits / self.n_classes))
+
+    def __iter__(self):
+      pass
+
+    def __len__(self):
+        return len(self.y)
+
+class DataModule(pl.LightningDataModule):
+  pass
+
+class ModBertBase(pl.LightningModule):
     
     def __init__(self, params, head_dropout = 0.2, num_classes = 5):
         super().__init__()
@@ -55,7 +107,6 @@ class BertBase(pl.LightningModule):
         self.batch_size = params['batch_size']
         self.loss = nn.CrossEntropyLoss()
         self.accuracy = Accuracy()
-        self.confusion_matrix = ConfusionMatrix(num_classes)
         self.model_str = params['pre_trained_model']
         self.save_hyperparameters()
 
@@ -72,7 +123,7 @@ class BertBase(pl.LightningModule):
         self.new_layers = nn.Sequential(*layers)
 
     def prepare_data(self):
-      tokenizer = AutoTokenizer.from_pretrained(self.model_str, trust_remote_code=True) # type: ignore
+      tokenizer = AutoTokenizer.from_pretrained(self.model_str, trust_remote_code=True, use_fast=True) # type: ignore
 
       tokens_train = tokenizer.batch_encode_plus(
           x_train.tolist(),
@@ -176,52 +227,65 @@ class BertBase(pl.LightningModule):
     def configure_optimizers(self):
       return torch.optim.AdamW(self.parameters(), lr=self.learning_rate)
 
+# if __name__ == '__main__':
 
-if __name__ == '__main__':
-    #Grabs the preprocessed data from the previous step:
-    preprocess_task = Task.get_task(task_name='data_split',
-                                    project_name=PROJECT_NAME)
+# %%
+
+#Grabs the preprocessed data from the previous step:
+preprocess_task = Task.get_task(task_name='data_split',
+                              project_name=PROJECT_NAME)
 
 
-    Path('data/interim').mkdir(parents=True, exist_ok=True)
+Path('data/interim').mkdir(parents=True, exist_ok=True)
 
-    train_data = preprocess_task.artifacts['train_data'].get()
-    test_data = preprocess_task.artifacts['test_data'].get()
-    valid_data = preprocess_task.artifacts['validation_data'].get()
+train_data = preprocess_task.artifacts['train_data'].get()
+test_data = preprocess_task.artifacts['test_data'].get()
+valid_data = preprocess_task.artifacts['validation_data'].get()
 
-    # train_data = pd.read_csv(train_path)
-    # test_data = pd.read_csv(test_path)
-    # valid_data = pd.read_csv(valid_path)
+# train_data = pd.read_csv(train_path)
+# test_data = pd.read_csv(test_path)
+# valid_data = pd.read_csv(valid_path)
 
-    local_data_path = Path(os.getcwd()) / 'data' 
+local_data_path = Path(os.getcwd()) / 'data' 
+
+# # #Defines training callbacks.
+model_name = parameters['pre_trained_model']
+model_path = local_data_path / 'models' / f'{model_name}'
+# model_path.mkdir(parents=True, exist_ok=True)
+
+# checkpoint_callback = ModelCheckpoint(
+#     monitor='val_loss',
+#     dirpath=str(model_path)
+#     )
+
+# #Trains the model.
+# x_train, x_val, x_test = train_data['text'], valid_data['text'], test_data['text']
+# y_train, y_val, y_test = train_data['label'], valid_data['label'], test_data['label']
+
+
+# model = BertBase(params=parameters)
+
+# # # # model = train_model(dm, parameters)
+# trainer = pl.Trainer(max_epochs=parameters['num_epochs'], accelerator=parameters['accelerator'], 
+#                      devices=parameters['devices'], logger=True)
+
+# trainer.fit(model)
+# trainer.save_checkpoint(f"{model_name}.ckpt")
+
+# # #Stores the trained model as an artifact (zip).:w
+# task.upload_artifact(checkpoint_callback.best_model_path, 'model_best_checkpoint')
+
+
+max_len = 128
+model_str = parameters['pre_trained_model']
+train_dataset = TokenizeDataset(train_data, max_len, model_str)
+valid_dataset = TokenizeDataset(valid_data, max_len, model_str, eval=True)
+test_dataset = TokenizeDataset(test_data, max_len, model_str, eval=True)
+
     
-    # # #Defines training callbacks.
-    model_name = parameters['pre_trained_model']
-    model_path = local_data_path / 'models' / f'{model_name}'
-    # model_path.mkdir(parents=True, exist_ok=True)
+# %%
+train_dataloader = DataLoader(train_dataset, batch_size=1)
+val_dataloader = DataLoader(valid_dataset, batch_size=1)
+test_dataloader = DataLoader(test_dataset, batch_size=1)
 
-    # checkpoint_callback = ModelCheckpoint(
-    #     monitor='val_loss',
-    #     dirpath=str(model_path)
-    #     )
-    
-    # #Trains the model.
-    x_train, x_val, x_test = train_data['text'], valid_data['text'], test_data['text']
-    y_train, y_val, y_test = train_data['label'], valid_data['label'], test_data['label']
-    model = BertBase(params=parameters)
-
-    # # # model = train_model(dm, parameters)
-    trainer = pl.Trainer(max_epochs=parameters['num_epochs'], accelerator=parameters['accelerator'], 
-                         devices=parameters['devices'], logger=True)
-    
-    trainer.fit(model)
-    trainer.save_checkpoint(f"{model_name}.ckpt")
-
-    # # #Stores the trained model as an artifact (zip).:w
-    # task.upload_artifact(checkpoint_callback.best_model_path, 'model_best_checkpoint')
-
-
-
-
-
-    
+# %%
