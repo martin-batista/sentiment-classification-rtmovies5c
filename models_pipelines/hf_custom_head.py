4ff6b45e65d08cb732f9e1eb232a700a14f0d939
martin-batista <mbatista.dev@gmail.com>
2022-07-24 21:01:10 -0300

    Debug: debug mode parameters.

--- models_pipelines/hf_custom_head.py
+++ models_pipelines/hf_custom_head.py
@@ -30,8 +30,8 @@ from pipe_conf import PROJECT_NAME
 
 parameters = {
         'pre_trained_model': 'bert-base-uncased',
-        'batch_size': 16,
-        'max_length': 256,
+        'batch_size': 64,
+        'max_length': 16,
         'lr': 2e-5,
         'num_epochs': 1,
         'stratified_sampling': True,
